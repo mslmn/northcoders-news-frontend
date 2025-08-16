7281dc05e1ba4dbd7c593b53af6463b8b8c0d1bc
mslmn <55067911+mslmn@users.noreply.github.com>
2025-08-16 11:15:29 +0100

    wrap app in userProvider; add Home route and scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-import "./App.css";
+import { Routes, Route, Navigate, useLocation, useNavigationType } from "react-router-dom";
+import { useEffect } from "react";
 import { UserProvider } from "./contexts/UserContext";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import ArticleList from "./components/ArticleList";
 import ArticleDetail from "./components/ArticleDetail";
-import { Route, Routes } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const location = useLocation();
+  const navType = useNavigationType();
+
+  useEffect(() => {
+    if (navType !== "POP") {
+      window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    }
+  }, [location.pathname, location.search, navType]);
+
   return (
-    <>
-      <UserProvider>
-        <main>
-          <Header />
-          <Routes>
-            <Route path="/" element={<ArticleList />} />
-            <Route path="/articles" element={<ArticleList />} />
-            <Route path="/articles/:articleId" element={<ArticleDetail />} />
-          </Routes>
-        </main>
-      </UserProvider>
-    </>
+    <UserProvider>
+      <div className="min-h-screen bg-base-200">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<ArticleList />} />
+          <Route path="/articles/:articleId" element={<ArticleDetail />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </UserProvider>
   );
 }
-
-export default App;
