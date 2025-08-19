816eb696dc0e9e3ac3c7a3919b12624bd3c31602
mslmn <55067911+mslmn@users.noreply.github.com>
2025-08-19 15:43:12 +0100

    comment handling

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,59 +1,81 @@
 const baseUrl = "https://northcoders-news-gida.onrender.com/api";
 
-export const getAllArticles = () => {
-  return fetch(`${baseUrl}/articles`)
+export const getTopics = () => {
+  return fetch(`${baseUrl}/topics`)
     .then((res) => {
-      if (!res.ok) {
-        return Promise.reject({ status: res.status, msg: "failed to fetch articles" });
-      }
+      if (!res.ok) return Promise.reject({ status: res.status, msg: "failed to fetch topics" });
       return res.json();
     })
-    .then(({ articles }) => {
-      return articles;
-    });
+    .then(({ topics }) => topics);
+};
+
+export const getAllArticles = (params = {}) => {
+  const qs = new URLSearchParams();
+  if (params.topic) qs.set("topic", params.topic);
+  if (params.sort_by) qs.set("sort_by", params.sort_by);
+  if (params.order) qs.set("order", params.order);
+
+  const url = qs.toString() ? `${baseUrl}/articles?${qs.toString()}` : `${baseUrl}/articles`;
+
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) return Promise.reject({ status: res.status, msg: "failed to fetch articles" });
+      return res.json();
+    })
+    .then(({ articles }) => articles);
 };
 
 export const getArticleById = (articleId) => {
   return fetch(`${baseUrl}/articles/${articleId}`)
     .then((res) => {
-      if (!res.ok) {
-        return Promise.reject({ status: res.status, msg: "failed to fetch article" });
-      }
+      if (!res.ok) return Promise.reject({ status: res.status, msg: "failed to fetch article" });
       return res.json();
     })
-    .then(({ article }) => {
-      return article;
-    });
+    .then(({ article }) => article);
 };
 
 export const getCommentsByArticleId = (articleId) => {
   return fetch(`${baseUrl}/articles/${articleId}/comments`)
     .then((res) => {
-      if (!res.ok) {
+      if (!res.ok)
         return Promise.reject({ status: res.status, msg: "failed to fetch article comments" });
-      }
       return res.json();
     })
-    .then(({ comments }) => {
-      return comments;
-    });
+    .then(({ comments }) => comments);
+};
+
+export const postComment = (articleId, { username, body }) => {
+  return fetch(`${baseUrl}/articles/${articleId}/comments`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, body }),
+  })
+    .then((res) => {
+      if (!res.ok) return Promise.reject({ status: res.status, msg: "failed to add comment" });
+      return res.json();
+    })
+    .then(({ comment }) => comment);
+};
+
+export const deleteCommentById = (commentId) => {
+  return fetch(`${baseUrl}/comments/${commentId}`, {
+    method: "DELETE",
+  }).then((res) => {
+    if (!res.ok) return Promise.reject({ status: res.status, msg: "failed to delete comment" });
+    return;
+  });
 };
 
 export const updateArticleVotes = (articleId, incVotes) => {
   return fetch(`${baseUrl}/articles/${articleId}`, {
     method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ inc_votes: incVotes }),
   })
     .then((res) => {
-      if (!res.ok) {
+      if (!res.ok)
         return Promise.reject({ status: res.status, msg: "failed to update article votes" });
-      }
       return res.json();
     })
-    .then(({ article }) => {
-      return article;
-    });
+    .then(({ article }) => article);
 };
