e0adbbaa1c707c02da611608632cbbdc1c907ddc
mslmn <55067911+mslmn@users.noreply.github.com>
2025-08-20 15:03:09 +0100

    update avatar url

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -14,8 +14,7 @@ export const UserProvider = ({ children }) => {
   const currentUser = {
     username: "grumpy19",
     name: "Paul Grump",
-    avatar_url:
-      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
+    avatar_url: "https://i.imgur.com/YqKO2hJ.jpeg",
   };
 
   return (
